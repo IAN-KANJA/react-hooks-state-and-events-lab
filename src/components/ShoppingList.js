822d3a5a2f3d0629142968db0c7e5fd5656d25aa
iannjau <lantanacreations@gmail.com>
2024-05-01 07:15:08 +0300

    add my file

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,21 @@
 import React from "react";
 import Item from "./Item";
 
+
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleCategoryChange(event) {
+    
+    setSelectedCategory(event.target.value);
+  }
+
+  
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+
+    return item.category === selectedCategory;
+  });
   return (
     <div className="ShoppingList">
       <div className="Filter">
